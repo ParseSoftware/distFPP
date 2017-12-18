5898238de903371808d2a6ae0f496f13821fd5e2
Ubuntu <ubuntu@ip-172-31-25-238.us-west-2.compute.internal>
2017-12-18 17:35:18 +0000

    working workfoce config

--- app.js
+++ app.js
@@ -6,28 +6,26 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(cors());
 
-app.options("/*", function(req, res, next){
+app.options('/*', function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.send(200);
 });
-  
+
 app.options('*', cors());
 
 //Import all routes
 
 app.set('port', 3000);
 
-app.use(function (req, res, next) {
-    if(!req.originalUrl.includes('/api/')){
-        res.sendfile(__dirname+'/public/index.html');
-    }else{
-        next();
-    }
-       
-  });
+app.use(express.static(__dirname + '/workforce/dist'));
+
+app.get('*', function(req, res) {
+       res.sendFile(__dirname+'/workforce/dist/index.html');
+})
+
 //here start the middlewares
 app.listen(app.get('port'), function() {
                 console.log('Express server listening on port ' + app.get('port'))
-            })
+  })
